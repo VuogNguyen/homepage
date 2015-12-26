57ec0c0f93ccf48bd26b14227c797a770d28c432
Vuong Nguyen <me@ngvuong.com>
2015-12-26 20:35:37 +0700

    Update about me template page, moving content to backend and adjust form max width

--- template-aboutme.php
+++ template-aboutme.php
@@ -20,10 +20,7 @@ get_header(); ?>
 
 			<div class="content">
 				<p class="avatar" style="background-image: url('<?php the_field('avatar_image', 'option'); ?>')"></p>
-				<p>Welcome to my personal website, where you can find my information exclusively here!</p>
-				<p>Let me introduce about myself. My name is Vuong Nguyen, a software engineer in Hochiminh City, Vietnam. I believe that I would bring an extremely valuable skill set to this interesting code generation project, along with my results driven focus and exceptional communication skills. Well-prepared at RMIT University, I am confident that I will succeed in the profession I have chosen.</p>
-				<p>In this website, you will find my Github Repo, my detailed project experiences, and my contact information. Should you need more information, please feel free to contact me at +84 907 822 788 or via email at me@ngvuong.com. I appreciate your time considering my credentials in this website.</p>
-				<p>Best regards</p>
+				<?php the_field('content'); ?>
 			</div>
 		</div>
 	</div>
